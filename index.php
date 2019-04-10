ea9fa652bf01599f9b0822091773153edbbacd5b
Paul59380 <paul.genly59380@gmail.com>
2019-04-10 16:58:46 +0200

    Create view for connexion and inscription add page for design

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
 require ('vendor/autoload.php');
 
-use model\Boat;
-use model\BoatManager;
-use model\User;
-use model\UserManager;
-use model\CityManager;
-use model\FluvialTripManager;
-use controller\BoatController;
-use controller\CityController;
-use controller\UserController;
+//Routing
+$page = 'connection';
+if(isset($_GET['action'])) {
+    $page = $_GET['action'];
+}
+
+//Render template
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/views');
+$twig = new Twig_Environment($loader, [
+    'cache' => false, // __DIR__ . '/tmp'
+]);
+// Recupération de données
+$fluvialTripController = \controller\FluvialTripController::getInstance();
+$fluvialTrips = $fluvialTripController->getListFluvialTrips();
+
+switch ($page) {
+    case 'connection':
+        echo $twig->render('connection.twig');
+        break;
+    case 'inscription':
+        echo $twig->render('inscription.twig');
+        break;
+    default:
+        header('HTTP/1.0 404 Not Found');
+        echo $twig->render('404.twig');
+        break;
+}
 
