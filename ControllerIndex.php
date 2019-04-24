ab8163226df36728d3c07a090a3f4663ed50cd58
Paul59380 <paul.genly59380@gmail.com>
2019-04-24 09:37:34 +0200

    Twig integration

---
+++ ControllerIndex.php
@@ -0,0 +1,215 @@
+<?php
+//use Exception;
+use model\User;
+
+class ControllerIndex
+{
+    protected $fluvialTripController;
+    protected $userController;
+    protected $cityController;
+    protected $boatController;
+    protected $favoriteController;
+    protected $loader;
+    protected $twig;
+
+    public function __construct()
+    {
+        $this->fluvialTripController = controller\FluvialTripController::getInstance();
+        $this->userController = controller\UserController::getInstance();
+        $this->cityController = controller\CityController::getInstance();
+        $this->boatController = controller\BoatController::getInstance();
+        $this->favoriteController = controller\FavoriteTransportController::getInstance();
+        $this->loader = new \Twig_Loader_Filesystem(__DIR__.'\views');
+        $this->twig = new \Twig_Environment($this->loader, [
+            'cache' => false
+        ]);
+    }
+
+    public function displayConnectForm(){
+        echo $this->twig->render('connection.php.twig');
+        if (!empty($_POST['name']) && !empty($_POST['firstName']) && !empty($_POST['password'])) {
+            if ($this->userController->existUser($_POST['name'], $_POST['firstName'])) {
+                $user = new User($this->userController->getUser($_POST['name'], $_POST['firstName']));
+                if (password_verify($_POST['password'], $user->getPass())) {
+                    $_SESSION = [
+                        "name" => $user->getName(),
+                        "firstName" => $user->getFirstname(),
+                        "id" => $user->getId(),
+                        "role" => $user->getId_role()
+                    ];
+                    if($_SESSION['role'] == 2){
+                        header('Location:index.php?action=homeUser&idUser=' . $_SESSION['id']);
+                    } elseif ( $_SESSION['role'] == 1 ){
+                        header('Location:index.php?action=admin&idUser=' . $_SESSION['id']);
+                    }
+                } else {
+                    echo "Mot de pass incorrect";
+                    unset($user);
+                }
+            } else {
+                echo "Compte inexistant";
+            }
+        }
+    }
+
+    public function displayInscriptionForm()
+    {
+        echo $this->twig->render('inscription.php.twig');
+        if (!empty($_POST['NewName']) && !empty($_POST['NewFirstName']) && !empty($_POST['NewPassword']) && !empty($_POST['PhoneNumber'])) {
+            if (!$this->userController->existUser($_POST['NewName'], $_POST['NewFirstName'])) {
+                $this->userController->addUser($_POST['NewName'], $_POST['NewFirstName'],$_POST['PhoneNumber'], password_hash($_POST['NewPassword'],PASSWORD_DEFAULT));
+                $user = new User($this->userController->getUser($_POST['NewName'], $_POST['NewFirstName']));
+                $_SESSION = [
+                    "name" => $user->getName(),
+                    "firstName" => $user->getFirstname(),
+                    "id" => $user->getId(),
+                    "role" => $user->getId_role()
+                ];
+                if($_SESSION['role'] == 2){
+                    header('Location:index.php?action=homeUser&idUser=' . $_SESSION['id']);
+                } elseif ( $_SESSION['role'] == 1 ){
+                    header('Location:index.php?action=admin&idUser=' . $_SESSION['id']);
+                }
+            } else {
+                echo "Ce compte existe déjà";
+            }
+        }
+    }
+
+    public function displayHomeUser()
+    {
+        echo $this->twig->render('userHome.php.twig', ['session' => [
+            'name' => $_SESSION['name'],
+            'firstName' => $_SESSION['firstName'],
+            'role' => $_SESSION['role'],
+            'id' => $_SESSION['id']
+        ]]);
+    }
+
+    public function displayHomeAdmin()
+    {
+        echo $this->twig->render('adminHome.php.twig', ['session' => [
+            'name' => $_SESSION['name'],
+            'firstName' => $_SESSION['firstName'],
+            'role' => $_SESSION['role'],
+            'id' => $_SESSION['id']
+        ]]);
+    }
+
+    public function displayUserTrips()
+    {
+        if (isset($_GET['idTrip'])) {
+            $getTrip = $this->fluvialTripController->getFluvialTrip($_GET['idTrip']);
+            $trips = $this->fluvialTripController->getListFluvialTrips();
+            $favoriteTrips = $this->favoriteController->getFavoriteTrip($_GET['idUser']);
+            echo $this->twig->render('userTrip.php.twig', ['getTrip' => $getTrip, 'session' => $_SESSION, 'trips' => $trips, 'favorites' => $favoriteTrips]);
+        } else {
+            $trips = $this->fluvialTripController->getListFluvialTrips();
+            $favoriteTrips = $this->favoriteController->getFavoriteTrip($_GET['idUser']);
+            echo $this->twig->render('userTrip.php.twig', ['session' => $_SESSION, 'trips' => $trips, 'favorites' => $favoriteTrips]);
+        }
+    }
+
+    public function displayFormAddTrip()
+    {
+        $trips = $this->fluvialTripController->getListFluvialTrips();
+        $cities = $this->cityController->getListCities();
+        //require('views/adminAddTrip.php.twig');
+        echo $this->twig->render('adminAddTrip.php.twig', ['session' => $_SESSION, 'trips' => $trips, 'cities' => $cities]);
+    }
+
+
+    public function adminAddTrip()
+    {
+        try{
+            $depart = (int)$_POST['departure_city'];
+            $finish = (int)$_POST['finishing_city'];
+            $departure = $this->cityController->getCity($depart);
+            $finishing = $this->cityController->getCity($finish);
+
+            $this->fluvialTripController->addFluvialTrip($departure->getName(),$departure->getLat(),$departure->getLon(),$finishing->getName(),$finishing->getLat(),$finishing->getLon(),$_POST['price_ton'],$_POST['weight'], $_POST['date_transport']);
+            header('Location:index.php?action=adminAddTrip');
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    public function addCity()
+    {
+        $this->cityController->addCity($_POST['city'], $_POST['lat'], $_POST['lon']);
+        header('Location:index.php?action=adminAddTrip');
+    }
+
+    public function displaySearchBoat()
+    {
+        $trips = $this->fluvialTripController->getListFluvialTrips();
+        echo $this->twig->render('adminSearch.php.twig', ['session' => $_SESSION, 'trips' => $trips]);
+    }
+
+    public function searchBoat()
+    {
+        $trip = $this->fluvialTripController->getFluvialTrip($_GET['idTrip']);
+        $boats = $this->boatController->getListBoat($_GET['capacity']);
+        echo $this->twig->render('adminBoat.php.twig', ['session' => $_SESSION, 'trip' => $trip, 'boats' => $boats]);
+    }
+
+    public function displayOwnerBoat()
+    {
+        $boats = $this->boatController->getOwnerBoat($_GET['idUser']);
+        echo $this->twig->render('userBoat.php.twig', ['session' => $_SESSION ,'boats' => $boats]);
+    }
+
+    public function addBoat()
+    {
+        $this->boatController->addBoat($_GET['idUser'], $_POST['name'], $_POST['capacity']);
+        header('Location:index.php?action=addBoat&idUser='.$_GET['idUser']);
+    }
+
+    public function addFavoriteTrip()
+    {
+        $this->favoriteController->addFavoriteTrip($_GET['idTrip'], $_GET['idUser']);
+        header('Location:'. $_SERVER["HTTP_REFERER"]);
+    }
+
+    public function deleteFavoriteTrip()
+    {
+        $this->favoriteController->deleteFavoriteTrip($_GET['idFavorite']);
+        header('Location:'. $_SERVER["HTTP_REFERER"]);
+    }
+
+    public function deleteBoat()
+    {
+        $this->boatController->deleteBoat($_GET['idBoat']);
+        header('Location:'. $_SERVER["HTTP_REFERER"]);
+    }
+
+    public function displayUpdateTrip()
+    {
+        $trip = $this->fluvialTripController->getFluvialTrip($_GET['idTrip']);
+        $cities = $this->cityController->getListCities();
+        echo $this->twig->render('adminUpdateTrip.php.twig', ['session' => $_SESSION, 'trip' => $trip, 'cities' => $cities]);
+    }
+
+    public function updateTrip()
+    {
+        $depart = (int)$_POST['departure_city'];
+        $finish = (int)$_POST['finishing_city'];
+        $departure = $this->cityController->getCity($depart);
+        $finishing = $this->cityController->getCity($finish);
+
+        $this->fluvialTripController->updateFluvialTrip($_GET['idTrip'], $departure->getName(), $departure->getLat(), $departure->getLon(), $finishing->getName(), $finishing->getLat(), $finishing->getLon(), $_POST['price_ton'], $_POST['weight'], $_POST['date_transport']);
+        header('Location:'. $_SERVER["HTTP_REFERER"]);
+    }
+
+    public function deleteTrip()
+    {
+        $this->fluvialTripController->deleteFluvialTrip($_GET['idTrip']);
+        header('Location:'. $_SERVER["HTTP_REFERER"]);
+    }
+
+    public function disconnection()
+    {
+        session_destroy();
+        header('Location:index.php');
+    }
+}
---
+++ tmp/82/82d265267b7e2201350539130fc6c495eae700488f05b3318ad76cd3a660cff4.php
@@ -0,0 +1,56 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* connection.php.twig */
+class __TwigTemplate_0cfe6ceb0a7109bf2ffec1fa5d3137d7aab4037d4cfff878ff8a913996889f4f extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "Hello les gens !
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "connection.php.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  30 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "connection.php.twig", "C:\\wamp64\\www\\P5\\views\\connection.php.twig");
+    }
+}
